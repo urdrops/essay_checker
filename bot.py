b7c8e41064347c611481ecf24d66b90a2a171ab4
Urdrops <urdropsss@gmail.com>
2023-07-30 22:02:53 +0500

    test 1

--- bot.py
+++ bot.py
@@ -2,32 +2,22 @@ import asyncio
 import logging
 
 from aiogram import Bot, Dispatcher
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.contrib.fsm_storage.redis import RedisStorage2
 
-from tgbot.config import load_config
+from tgbot.config import load_config, Config
 from tgbot.filters.admin import AdminFilter
-from tgbot.handlers.admin import register_admin
 from tgbot.handlers.echo import register_echo
-from tgbot.handlers.user import register_user
-from tgbot.middlewares.environment import EnvironmentMiddleware
+from tgbot.handlers.admin import register_admin
 
 logger = logging.getLogger(__name__)
 
 
-def register_all_middlewares(dp, config):
-    dp.setup_middleware(EnvironmentMiddleware(config=config))
+def register_all_handlers(dp):
+    register_echo(dp)
+    register_admin(dp)
 
 
 def register_all_filters(dp):
     dp.filters_factory.bind(AdminFilter)
-
-
-def register_all_handlers(dp):
-    register_admin(dp)
-    register_user(dp)
-
-    register_echo(dp)
 
 
 async def main():
@@ -35,19 +25,14 @@ async def main():
         level=logging.INFO,
         format=u'%(filename)s:%(lineno)d #%(levelname)-8s [%(asctime)s] - %(name)s - %(message)s',
     )
-    logger.info("Starting bot")
-    config = load_config(".env")
+    logger.info("starting bot")
+    config: Config = load_config(".env")
 
-    storage = RedisStorage2() if config.tg_bot.use_redis else MemoryStorage()
     bot = Bot(token=config.tg_bot.token, parse_mode='HTML')
-    dp = Dispatcher(bot, storage=storage)
+    dp = Dispatcher(bot)
 
     bot['config'] = config
-
-    register_all_middlewares(dp, config)
-    register_all_filters(dp)
     register_all_handlers(dp)
-
     # start
     try:
         await dp.start_polling()
@@ -57,8 +42,8 @@ async def main():
         await bot.session.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         asyncio.run(main())
     except (KeyboardInterrupt, SystemExit):
-        logger.error("Bot stopped!")
+        logger.error("bot stopped!")
--- tgbot/handlers/admin.py
+++ tgbot/handlers/admin.py
@@ -3,8 +3,8 @@ from aiogram.types import Message
 
 
 async def admin_start(message: Message):
-    await message.reply("Hello, admin!")
+    await message.reply("wassup my nigga?")
 
 
 def register_admin(dp: Dispatcher):
-    dp.register_message_handler(admin_start, commands=["start"], state="*", is_admin=True)
+    dp.register_message_handler(admin_start, commands=['start'], is_admin=True)
